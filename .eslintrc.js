42b64e36b805a5199d7bf8dfd51d519b55e721b4
Jude Okafor <ebuka@kippa.africa>
2022-06-27 21:18:14 +0100

    [add] base setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,18 @@ module.exports = {
   parserOptions: {
     sourceType: 'module',
   },
+   root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     'prettier/prettier': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   plugins: ['@typescript-eslint', 'prettier'],
 }
